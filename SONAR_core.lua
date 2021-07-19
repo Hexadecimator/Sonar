c2be10632f6253b37927a15635c553956d6f1efa
Logan <logan.scholz@gmail.com>
2021-07-19 09:37:53 -0700

    many updates

--- SONAR_core.lua
+++ SONAR_core.lua
@@ -1,21 +1,24 @@
--- **************************************************
+-- ***********************************************************
 -- Addon: SONAR
--- Description: alternate tracking type every GCD
--- Author: MarxWasRight-Fairbanks
+-- Description: alternate minimap tracking type every GCD
+-- Author: MarxWasRight-Fairbanks[H]
 --
 -- TODO:
 -- 1. Add an event listener for entering combat and stop the
---    looping because it eats GCDs
+--    looping because it eats GCDs [DONE]
 -- 2. XML GUI interface to control which tracking types are
 --    cycled through
 -- 3. Disable addon if current character only has 1 or less
---    tracking types
+--    tracking types [DONE]
 --
--- **************************************************
+-- ***********************************************************
 
 local AddOn, SONARcore = ...;
 
-local slfgBTNState = false;
+-- ***********************************
+-- **** START GLOBAL DEFINITIONS *****
+-- ***********************************
+
 local sonarRunning = false;
 local sonarCurrID = 0;
 local sonarTrackerID = {};
@@ -36,18 +39,16 @@ end
 function initializeSonar(self)
     --
     local tc = GetNumTrackingTypes();
-    print("num tracking type available: " .. tc);
+    print("|cffF94F97num tracking type available: |r" .. tc);
     --
     for i=1,tc do
-        --stuff
         --name, texture, active, category, nested = GetTrackingInfo(i);
         name, _, _, category, _ = GetTrackingInfo(i);
 
         if category == "spell" then
             sonarIDX = sonarIDX + 1;
-            --sonarTrackerTypes[sonarIDX] = name;
             sonarTrackerID[sonarIDX] = i;
-            print(name .. " added to sonarTrackerTypes. sonarIDX = " .. sonarIDX);
+            print(name .. "|cffF94F97 added to sonarTrackerTypes. sonarIDX = |r" .. sonarIDX);
         end
     end
 
@@ -56,26 +57,24 @@ function initializeSonar(self)
     --(skillType, special will be nil, nil if spell not found)
     skillType, special = GetSpellBookItemInfo("Find Fish");
 
-    if (skillType == nil) then 
+    if (skillType == nil) then
         --print("No fishing"); 
         sonarHasFishing = false;
-    end
-
-    if (skillType ~= nil) then 
+    elseif(skillType ~= nil) then
         --print("Fisherman detected");
         sonarHasFishing = true;
     end
-    
+
     -- if we found trackable stuff intialize sonarCurrID
-    if (sonarIDX > 0 or sonarHasFishing == true) then sonarCurrID = 1; end
+    if ((sonarIDX > 1) or (sonarIDX == 1 and sonarHasFishing == true)) then
+        sonarCurrID = 1;
+    end
 
     -- if we found nothing then turn off the addon
-    if (sonarIDX <= 0 and sonarHasFishing == false) then 
-        sonarRunning = False; 
-        print("|cff377CF0NO TRACKING DETECTED - SONAR DISABLED|r");
+    if ((sonarIDX <= 1 and sonarHasFishing == false) or (sonarIDX <= 0 and sonarHasFishing == true)) then
+        sonarRunning = false;
+        print("|cffF94F97NO TRACKING DETECTED - SONAR DISABLED|r");
     end
-    
-    -- TODO: disable the addon if they have ONLY 1 gathering profession
 
     --[[
     print(" -- Final sonarTrackerTypes contents -- ");
@@ -87,25 +86,27 @@ function initializeSonar(self)
 end
 
 function cycleMinimapTracker(self)
-    --R: 0x37
-    --G: 0x7C
-    --B: 0xF0
-    --print("|cff377CF0TIMER TICKING|r"); -- debug
+    --R: 0xF9
+    --G: 0x4F
+    --B: 0x97
     if sonarRunning == true then
         if sonarStarting == false then
-            sonarCurrID = sonarCurrID + 1;
             if (sonarCurrID > sonarIDX) then
                 sonarCurrID = 1;
                 if sonarHasFishing == true then
                     CastSpellByName("Find Fish");
-                    return;
+                    return; --break out of this function
                 end
             end
             SetTracking(sonarCurrID,true);
+            sonarCurrID = sonarCurrID + 1;
         elseif sonarStarting == true then
             sonarStarting = false;
             if (sonarIDX <= 0 and sonarHasFishing == true) then
-                print("only fishing detected, shutting down");
+                --we may not needs this check anymore since we detect
+                --# of tracking types in the initializer function
+                --(tracked by variable sonarRunning)
+                print("|cffF94F97only fishing detected, shutting down|r");
                 CastSpellByName("Find Fish");
                 sonarRunning = false;
             end
@@ -117,73 +118,47 @@ function cycleMinimapTracker(self)
     end   
 end
 
-
-
-
--- **********************************************
--- ** SLASH CMD DEF SECTION *********************
--- **********************************************
-
--- quick access to frame stack for debugging
-SLASH_FRAMESTK1 = "/fs";
-SlashCmdList.FRAMESTK = function()
-    LoadAddOn("Blizzard_DebugTools");
-    FrameStackTooltip_Toggle();
-end
-
-SLASH_SLFG1 = "/slfg";
-SlashCmdList["SLFG"] = function(msg)
-    msg = string.upper(msg);
-    if (msg == "HELP") then
-        print("--Silence LFG Channel Addon--");
-        print("'/slfg hide' to hide SLFG");
-        print("'/slfg UL' to unlock the frame");
-        print("'/slfg L' to lock the frame");
-        print("'/slfg show' to show SLFG");
-        print("'/slfg join' to join LFG");
-        print("'/slfg leave' to leave LFG");
-    elseif (msg == "SHOW") then
-        UItogBtn:Show();
-        print("SHOW");
-    elseif (msg == "HIDE") then
-        UItogBtn:Hide();
-        print("HIDE");
-    elseif (msg == "JOIN") then
-        local channel_type, channel_name = JoinChannelByName("LookingForGroup");
-        ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, "LookingForGroup");
-        print("JOIN");
-    elseif (msg == "LEAVE") then
-        LeaveChannelByName("LookingForGroup");
-        print("LEAVE");
-    elseif (msg == "UL") then
-        -- TODO: toggle lock for position of SLFG's main frame
-        UItogBtn:SetMovable(true);
-        UItogBtn:RegisterForDrag("LeftButton");
-        UItogBtn:SetScript("OnDragStart", UItogBtn.StartMoving);
-        UItogBtn:SetScript("OnDragStop", UItogBtn.StopMovingOrSizing);
-        UItogBtn:SetUserPlaced(true);
-        print("SLFG FRAME UNLOCKED");
-    elseif (msg == "L") then
-        UItogBtn:StopMovingOrSizing();
-        UItogBtn:RegisterForDrag();
-        UItogBtn:SetMovable(false);
-        print("SLFG FRAME LOCKED");
-    elseif (msg == "Q") then
-        print("W-H-L-R");
-        print(UItogBtn:GetWidth());
-        print(UItogBtn:GetHeight());
-        print(UItogBtn:GetLeft());
-        print(UItogBtn:GetRight());
-    elseif (msg == "S") then
-        sonarRunning = true;
-        sonarTimer = C_Timer.NewTicker(1.5, cycleMinimapTracker)
-    elseif (msg == "E") then
-        --need to end the timer here    
-        if sonarTimer ~= null then
+function stopSonar(self)
+    -- stop the addon from eating up GCDs when the player
+    -- enters combat
+    if (sonarTimer ~= nil) then
+        if (sonarTimer:IsCancelled() == false) then
             sonarTimer:Cancel();
+            print("|cffFF0000" .. "SONAR TIMER CANCELLED" .. "|r");
         end
-        --print("Timer cancelled: " .. sonarTimer:IsCancelled());
-    elseif (msg == "TRACKTYPE") then
-        --
+        
     end
 end
+
+
+-- **********************************************
+-- *********** SLASH CMD DEF SECTION ************
+-- **********************************************
+
+SLASH_SONAR1 = "/snr";
+SlashCmdList["SONAR"] = function(msg)
+    msg = string.upper(msg);
+    if (msg == "S") then
+        if sonarRunning == true then -- we've detected there are at least 2 trackers to switch between
+            sonarTimer = C_Timer.NewTicker(1.5, cycleMinimapTracker)
+        end
+    elseif (msg == "E") then
+        --need to end the timer here
+        stopSonar();
+    elseif (msg == "TRACKTYPE") then
+        if sonarRunning == false then
+            print("|cffF94F97SONAR did not detect enough tracking types!|r")
+        else
+            print("|cffF94F97" .. " -- SONAR DETECTED TRACKERS -- " .. "|r");
+            for i=1,sonarIDX do
+                print("|cffF94F00" .. sonarTrackerID[i] .. "|r");
+            end
+        end
+    else
+        print("|cffF94F97 -- SONAR ADDON HELP -- |r");
+        print("|cffF94F00'/snr S' to start Sonar|r");
+        print("|cffF94F00'/snr E' to stop Sonar|r");
+        print("|cffF94F00'/snr TRACKTYPE' to print detected tacking types|r");
+    end
+end
+
